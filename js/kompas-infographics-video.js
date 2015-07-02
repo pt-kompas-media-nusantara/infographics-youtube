4b5d7a6d748b19b1243e280b4f8ada2beecbb345
Yosef Yudha Wijaya <semboyan35@gmail.com>
2015-07-02 14:13:55 +0700

    added clickNavHandler() to handle navigation buttons' events

--- js/kompas-infographics-video.js
+++ js/kompas-infographics-video.js
@@ -97,15 +97,74 @@ function onYouTubePlayerReady() {
 			menuWrapper = document.createElement('div'),
 			navNext = document.createElement('div'),
 			navPrev = document.createElement('div'),
+			navCounter = 0,
+			maxCounter = Math.floor(ytData.length / 4),
+			minCounter = 0,
+			clickNavHandler = function (e) {
+				var btn = this,
+					menuWidth = menu.offsetWidth;
+				// console.log(ytData.length);
+				if (btn.classList.contains('next')) {
+					if (navCounter < maxCounter) {
+						navCounter++;
+					} else {
+						navCounter = maxCounter;
+					}
+				}
+
+				if (btn.classList.contains('prev')) {
+					if (navCounter > minCounter) {
+						navCounter--;
+					} else {
+						navCounter = minCounter;
+					}
+				}
+
+				menuList.style.left = -(navCounter * menuWidth) + 'px';
+			},
 			insertMenuItems = function(menuContainer) {
 				var itemsFrag = document.createDocumentFragment(),
 					menuWidth = menu.offsetWidth;
 					
 				menuContainer.style.width = ((menuWidth / 4) * ytData.length) + 'px';
-				console.log(menuWidth);
-				ytData.forEach(function(element) {
+				
+				ytData.forEach(function(element, index) {
+					var items = document.createElement('li'),
+						links = document.createElement('a'),
+						title = document.createElement('span'),
+						speakers = document.createElement('span'),
+						timeStart = document.createElement('span'),
+						box = document.createElement('span');
 
+					items.className = 'kompas-infographics__yt__timeline__list__items';
+					items.style.width = (menuWidth / 4) + 'px';
+					items.setAttribute('data-start', element.secondStart);
+
+					links.className = 'kompas-infographics__yt__timeline__list__items__links';
+					links.href = 'https://www.youtube.com/watch?v=' + ytVideoId;
+					links.target = '_blank';
+					links.setAttribute('data-id', index);
+
+					title.className = 'kompas-infographics__yt__timeline__list__items__titles';
+					title.textContent = element.title;
+					speakers.className = 'kompas-infographics__yt__timeline__list__items__speakers';
+					speakers.textContent = element.speaker;
+					timeStart.className = 'kompas-infographics__yt__timeline__list__items__time__start';
+					timeStart.textContent = element.start;
+					box.className = 'kompas-infographics__yt__timeline__list__items__box';
+
+
+					links.appendChild(title);
+					links.appendChild(speakers);
+					links.appendChild(timeStart);
+					links.appendChild(box);
+
+					items.appendChild(links);
+
+					itemsFrag.appendChild(items);
 				});
+
+				menuContainer.appendChild(itemsFrag);
 			};
 
 		menu.className = 'kompas-infographics__yt__timeline__menu';
@@ -123,6 +182,9 @@ function onYouTubePlayerReady() {
 		parent.insertBefore(menuWrapper, container);
 		
 		insertMenuItems(menuList);
+
+		navNext.addEventListener('click', clickNavHandler, false);
+		navPrev.addEventListener('click', clickNavHandler, false);
 	}
 
 }
